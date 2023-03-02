f1321a77dbf5161755429f8870250ca13adc237a
Arthur Hayrapetyan <ahayrapetyan@AHayrapetyanL1M.local>
2023-03-02 18:10:36 +0400

    Renamed sample apps

---
+++ samples/WebRTC-Sample-Krisp-IOS/WebRTC-Sample-Krisp-IOS-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  WebRTC-Sample-Krisp-IOS-Bridging-Header.h
+//  WebRTC-Sample-Krisp-IOS
+//
+//  Created by Arthur Hayrapetyan on 20.02.23.
+//
+
+#ifndef WebRTC_Sample_Krisp_IOS_Bridging_Header_h
+#define WebRTC_Sample_Krisp_IOS_Bridging_Header_h
+
+#import "KrispAudioProcessor/KrispAudioProcessor.h"
+
+#endif /* WebRTC_Sample_Krisp_IOS_Bridging_Header_h */
